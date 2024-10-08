71a16b52117a1af88db043f7a586f9688d12d5ec
Your Name <fintanoshea8@gmail.com>
2024-10-08 16:39:23 +1100

    Exercises adding correctly and persisting information input. Style needs to be updated

--- screens/AddExerciseModal.js
+++ screens/AddExerciseModal.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, FlatList, Modal, TextInput } from 'react-native';
+import { View, Text, TouchableOpacity, FlatList, Modal, TextInput, StyleSheet } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 import { Picker } from '@react-native-picker/picker';
 import { bodyParts, categories, exercises } from '../exercises';
 
-const AddExerciseModal = ({ setExerciseModalVisible, styles, onAddExercises }) => {
+const AddExerciseModal = ({ setExerciseModalVisible, onAddExercises }) => {
   const [searchQuery, setSearchQuery] = useState('');
   const [selectedBodyPart, setSelectedBodyPart] = useState('Any Body Part');
   const [selectedCategory, setSelectedCategory] = useState('Any Category');
@@ -64,69 +64,140 @@ const AddExerciseModal = ({ setExerciseModalVisible, styles, onAddExercises }) =
         setExerciseModalVisible(false);
       }}
     >
-      <View style={styles.modalView}>
-        <View style={styles.modalHeader}>
-          <TouchableOpacity onPress={() => setExerciseModalVisible(false)}>
-            <FontAwesome name="close" size={24} color="black" />
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={submitSelectedExercises}
-            disabled={selectedExercises.length === 0}
-            style={[styles.addButton, selectedExercises.length === 0 && styles.disabledButton]}
-          >
-            <Text style={styles.addButtonText}>Add</Text>
-          </TouchableOpacity>
+      <View style={styles.modalBackground}>
+        <View style={styles.modalView}>
+          <View style={styles.modalHeader}>
+            <TouchableOpacity onPress={() => setExerciseModalVisible(false)}>
+              <FontAwesome name="close" size={24} color="black" />
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={submitSelectedExercises}
+              disabled={selectedExercises.length === 0}
+              style={[styles.addButton, selectedExercises.length === 0 && styles.disabledButton]}
+            >
+              <Text style={styles.addButtonText}>Add</Text>
+            </TouchableOpacity>
+          </View>
+          <TextInput
+            style={styles.searchBar}
+            placeholder="Search exercises"
+            value={searchQuery}
+            onChangeText={setSearchQuery}
+          />
+          <View style={styles.filterButtonsContainer}>
+            <Picker
+              selectedValue={selectedBodyPart}
+              style={styles.filterButton}
+              onValueChange={(itemValue) => setSelectedBodyPart(itemValue)}
+            >
+              <Picker.Item label="Any Body Part" value="Any Body Part" />
+              <Picker.Item label="Core" value="Core" />
+              <Picker.Item label="Arms" value="Arms" />
+              <Picker.Item label="Back" value="Back" />
+              <Picker.Item label="Chest" value="Chest" />
+              <Picker.Item label="Legs" value="Legs" />
+              <Picker.Item label="Shoulders" value="Shoulders" />
+              <Picker.Item label="Other" value="Other" />
+              <Picker.Item label="Full Body" value="Full Body" />
+              <Picker.Item label="Cardio" value="Cardio" />
+            </Picker>
+            <Picker
+              selectedValue={selectedCategory}
+              style={styles.filterButton}
+              onValueChange={(itemValue) => setSelectedCategory(itemValue)}
+            >
+              <Picker.Item label="Any Category" value="Any Category" />
+              <Picker.Item label="Barbell" value="Barbell" />
+              <Picker.Item label="Dumbbell" value="Dumbbell" />
+              <Picker.Item label="Machine" value="Machine" />
+              <Picker.Item label="Weighted Bodyweight" value="Weighted Bodyweight" />
+              <Picker.Item label="Assisted Bodyweight" value="Assisted Bodyweight" />
+              <Picker.Item label="Cardio" value="Cardio" />
+              <Picker.Item label="Duration" value="Duration" />
+            </Picker>
+          </View>
+          <FlatList
+            data={filteredExercises}
+            renderItem={renderWorkoutExercise}
+            keyExtractor={(item) => item.name}
+          />
         </View>
-        <FlatList
-          ListHeaderComponent={
-            <View>
-              <TextInput
-                style={styles.searchBar}
-                placeholder="Search exercises"
-                value={searchQuery}
-                onChangeText={setSearchQuery}
-              />
-              <View style={styles.filterButtonsContainer}>
-                <Picker
-                  selectedValue={selectedBodyPart}
-                  style={styles.filterButton}
-                  onValueChange={(itemValue) => setSelectedBodyPart(itemValue)}
-                >
-                  <Picker.Item label="Any Body Part" value="Any Body Part" />
-                  <Picker.Item label="Core" value="Core" />
-                  <Picker.Item label="Arms" value="Arms" />
-                  <Picker.Item label="Back" value="Back" />
-                  <Picker.Item label="Chest" value="Chest" />
-                  <Picker.Item label="Legs" value="Legs" />
-                  <Picker.Item label="Shoulders" value="Shoulders" />
-                  <Picker.Item label="Other" value="Other" />
-                  <Picker.Item label="Full Body" value="Full Body" />
-                  <Picker.Item label="Cardio" value="Cardio" />
-                </Picker>
-                <Picker
-                  selectedValue={selectedCategory}
-                  style={styles.filterButton}
-                  onValueChange={(itemValue) => setSelectedCategory(itemValue)}
-                >
-                  <Picker.Item label="Any Category" value="Any Category" />
-                  <Picker.Item label="Barbell" value="Barbell" />
-                  <Picker.Item label="Dumbbell" value="Dumbbell" />
-                  <Picker.Item label="Machine" value="Machine" />
-                  <Picker.Item label="Weighted Bodyweight" value="Weighted Bodyweight" />
-                  <Picker.Item label="Assisted Bodyweight" value="Assisted Bodyweight" />
-                  <Picker.Item label="Cardio" value="Cardio" />
-                  <Picker.Item label="Duration" value="Duration" />
-                </Picker>
-              </View>
-            </View>
-          }
-          data={filteredExercises}
-          renderItem={renderWorkoutExercise}
-          keyExtractor={(item) => item.name}
-        />
       </View>
     </Modal>
   );
 };
+
+const styles = StyleSheet.create({
+  modalBackground: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+  },
+  modalView: {
+    width: '90%',
+    height: '80%', // Adjust the height to ensure it doesn't stretch too much
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 20,
+    alignItems: 'center',
+  },
+  modalHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    marginBottom: 20,
+  },
+  addButton: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+  },
+  disabledButton: {
+    backgroundColor: 'gray',
+  },
+  addButtonText: {
+    color: 'white',
+  },
+  searchBar: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingLeft: 10,
+    width: '100%',
+  },
+  filterButtonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    marginBottom: 20,
+  },
+  filterButton: {
+    flex: 1,
+    height: 50,
+  },
+  tableRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 5,
+  },
+  tableHeader: {
+    fontWeight: 'bold',
+    flex: 1,
+    textAlign: 'center',
+  },
+  tableCell: {
+    flex: 1,
+    textAlign: 'center',
+  },
+  tableInput: {
+    flex: 1,
+    textAlign: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: 'gray',
+  },
+});
 
 export default AddExerciseModal;
--- screens/ExerciseModal.js
+++ screens/ExerciseModal.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Button, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Modal, FlatList, CheckBox } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
-import AddExerciseModal from './AddExerciseModal'; // Assuming you have this component
+import AddExerciseModal from './AddExerciseModal'; // Ensure this is the correct path
 
 const getTimeOfDay = () => {
   const hours = new Date().getHours();
@@ -16,6 +16,7 @@ const ExerciseModal = ({ setModalVisible }) => {
   const [startTime] = useState(Date.now());
   const [elapsedTime, setElapsedTime] = useState(0);
   const [notes, setNotes] = useState('');
+  const [selectedExercises, setSelectedExercises] = useState([]);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -26,6 +27,38 @@ const ExerciseModal = ({ setModalVisible }) => {
 
   const minutes = Math.floor(elapsedTime / 60);
   const seconds = elapsedTime % 60;
+
+  const handleAddExercises = (exercises) => {
+    setSelectedExercises((prevExercises) => [...prevExercises, ...exercises]);
+    setExerciseModalVisible(false);
+  };
+
+  const renderSelectedExercise = ({ item }) => (
+    <View style={styles.selectedExerciseContainer}>
+      <View style={styles.exerciseHeader}>
+        <Text style={styles.exerciseName}>{item.name}</Text>
+        <TouchableOpacity style={styles.moreButton}>
+          <FontAwesome name="ellipsis-h" size={24} color="black" />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.table}>
+        <View style={styles.tableRow}>
+          <Text style={styles.tableHeader}>Set</Text>
+          <Text style={styles.tableHeader}>Previous</Text>
+          <Text style={styles.tableHeader}>kg</Text>
+          <Text style={styles.tableHeader}>Reps</Text>
+          <Text style={styles.tableHeader}>✓</Text>
+        </View>
+        <View style={styles.tableRow}>
+          <Text style={styles.tableCell}>1</Text>
+          <Text style={styles.tableCell}></Text>
+          <TextInput style={styles.tableInput} keyboardType="numeric" />
+          <TextInput style={styles.tableInput} keyboardType="numeric" />
+          <CheckBox value={false} />
+        </View>
+      </View>
+    </View>
+  );
 
   return (
     <View style={styles.container}>
@@ -45,6 +78,12 @@ const ExerciseModal = ({ setModalVisible }) => {
         placeholderTextColor="gray"
         value={notes}
         onChangeText={setNotes}
+        multiline={true}
+      />
+      <FlatList
+        data={selectedExercises}
+        renderItem={renderSelectedExercise}
+        keyExtractor={(item) => item.name}
       />
       <TouchableOpacity style={styles.addButton} onPress={() => setExerciseModalVisible(true)}>
         <Text style={styles.addButtonText}>Add Exercises</Text>
@@ -53,16 +92,17 @@ const ExerciseModal = ({ setModalVisible }) => {
         <Text style={styles.cancelButtonText}>Cancel Workout</Text>
       </TouchableOpacity>
 
-      {exerciseModalVisible && (
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={exerciseModalVisible}
+        onRequestClose={() => setExerciseModalVisible(false)}
+      >
         <AddExerciseModal
           setExerciseModalVisible={setExerciseModalVisible}
-          onAddExercises={(selectedExercises) => {
-            // Handle adding exercises
-            setExerciseModalVisible(false);
-          }}
-          styles={styles}
+          onAddExercises={handleAddExercises}
         />
-      )}
+      </Modal>
     </View>
   );
 };
@@ -97,43 +137,87 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 10,
-    textAlign: 'center', // Center the text
   },
   timer: {
     fontSize: 18,
     color: 'gray',
     marginBottom: 20,
-    textAlign: 'center', // Center the text
   },
   notesInput: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
+    width: '100%',
     marginBottom: 20,
     paddingLeft: 10,
     color: 'black',
   },
+  selectedExerciseContainer: {
+    backgroundColor: 'white',
+    padding: 10,
+    marginVertical: 5,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: 'lightgray',
+  },
+  exerciseHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  exerciseName: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  moreButton: {
+    padding: 5,
+  },
+  table: {
+    width: '100%',
+  },
+  tableRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 5,
+  },
+  tableHeader: {
+    fontWeight: 'bold',
+    flex: 1,
+    textAlign: 'center',
+  },
+  tableCell: {
+    flex: 1,
+    textAlign: 'center',
+  },
+  tableInput: {
+    flex: 1,
+    textAlign: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: 'gray',
+  },
   addButton: {
-    backgroundColor: '#89CFF0',
+    backgroundColor: '#89CFF0', // Lighter blue background
     padding: 10,
     borderRadius: 5,
     marginVertical: 10,
+    alignItems: 'center',
   },
   cancelButton: {
-    backgroundColor: '#ffcccc', // Light red color
+    backgroundColor: '#ff6666', // Slightly more red color
     padding: 10,
     borderRadius: 5,
     marginVertical: 10,
+    alignItems: 'center',
   },
   addButtonText: {
-    color: '#0096FF',
+    color: '#003366', // Dark blue text
     fontWeight: 'bold',
-    textAlign: 'center', // Center the text
+    textTransform: 'capitalize',
   },
   cancelButtonText: {
-    color: 'red',
+    color: 'white',
     fontWeight: 'bold',
-    textAlign: 'center', // Center the text
+    textTransform: 'capitalize',
   },
 });
 
