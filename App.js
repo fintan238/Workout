5b02626be78d9b947da5b60db51a907e19c7d44f
Your Name <fintanoshea8@gmail.com>
2024-10-11 15:18:22 +1100

    Revert "Firebase integrated. Files moved so that firebase works as tutorials present. Old code (HTML/React) commented out to only show "Hello World". TODO: Get auth working and add back old style and components (profile, history, workout pages etc)"
    This reverts commit 747e2bbb32354099b094f36539f3c2a0a196939f.

---
+++ App.js
@@ -0,0 +1,51 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import ProfileScreen from './screens/ProfileScreen';
+import WorkoutScreen from './screens/WorkoutScreen';
+import HistoryScreen from './screens/HistoryScreen';
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+
+            if (route.name === 'Profile') {
+              iconName = 'user';
+            } else if (route.name === 'Workout') {
+              iconName = 'plus';
+            } else if (route.name === 'History') {
+              iconName = 'history';
+            }
+
+            // You can return any component that you like here!
+            return <FontAwesome name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+        <Tab.Screen name="Workout" component={WorkoutScreen} />
+        <Tab.Screen name="History" component={HistoryScreen} />
+      </Tab.Navigator>
+      <StatusBar style="auto" />
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
--- src/googleSigninConfig.js
+++
@@ -1,6 +0,0 @@
-// googleSignInConfig.js
-import { GoogleSignin } from '@react-native-google-signin/google-signin';
-
-GoogleSignin.configure({
-  webClientId: 'YOUR_WEB_CLIENT_ID', // From Firebase Console
-});
